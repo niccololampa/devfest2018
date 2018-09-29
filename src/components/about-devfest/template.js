437387b31ee8dfee73daac15581b66a7d77be825
Stephen John Raymundo <bayan.mahal@live.com>
2018-09-29 21:26:04 +0800

    update about-devfest template

--- src/components/about-devfest/template.js
+++ src/components/about-devfest/template.js
@@ -1,6 +1,9 @@
 const template = (html) => html`
 
   <h1>About Devfest Section</h1>
+  <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque varius morbi enim nunc faucibus. Tellus rutrum tellus pellentesque eu tincidunt tortor. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus et. Egestas maecenas pharetra convallis posuere morbi leo urna. Lacus sed turpis tincidunt id aliquet risus. Est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. Tristique et egestas quis ipsum suspendisse ultrices gravida. Elementum eu facilisis sed odio morbi quis commodo odio. Pretium fusce id velit ut tortor. Vel turpis nunc eget lorem dolor sed viverra ipsum. Faucibus et molestie ac feugiat sed. Pellentesque nec nam aliquam sem et tortor. Nec ultrices dui sapien eget. Eu volutpat odio facilisis mauris sit amet massa vitae. Commodo quis imperdiet massa tincidunt nunc. Pulvinar mattis nunc sed blandit libero volutpat sed cras ornare. Faucibus purus in massa tempor nec feugiat. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Enim sit amet venenatis urna cursus eget nunc.
+  </p>
 
 `;
 
