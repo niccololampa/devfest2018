be10ba0b49a42b95e65087c486e54e9c9ef5ea42
Stephen John Raymundo <bayan.mahal@live.com>
2018-09-29 22:19:12 +0800

    update about-devfest component

--- src/components/about-devfest/template.js
+++ src/components/about-devfest/template.js
@@ -1,9 +1,15 @@
 const template = (html) => html`
 
-  <h1>About Devfest Section</h1>
-  <p>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque varius morbi enim nunc faucibus. Tellus rutrum tellus pellentesque eu tincidunt tortor. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus et. Egestas maecenas pharetra convallis posuere morbi leo urna. Lacus sed turpis tincidunt id aliquet risus. Est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. Tristique et egestas quis ipsum suspendisse ultrices gravida. Elementum eu facilisis sed odio morbi quis commodo odio. Pretium fusce id velit ut tortor. Vel turpis nunc eget lorem dolor sed viverra ipsum. Faucibus et molestie ac feugiat sed. Pellentesque nec nam aliquam sem et tortor. Nec ultrices dui sapien eget. Eu volutpat odio facilisis mauris sit amet massa vitae. Commodo quis imperdiet massa tincidunt nunc. Pulvinar mattis nunc sed blandit libero volutpat sed cras ornare. Faucibus purus in massa tempor nec feugiat. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Enim sit amet venenatis urna cursus eget nunc.
-  </p>
+  <section class="section">
+  <div class="section-container">
+    <span class="about-header">About Devfest Philippines</span>
+    <p>
+    Establishing GDG Philippines as a premiere lead tech community that helps further develop the innate Filipino innovation by exploring the possibilities and empowering minds, thus making an impact to the Philippines and to the world. GDG Philippines is set to brought up once again the Grandest technology of the year - DevFest Philippines 2018.
+    <br><br>
+    DevFest 2018 Philippines brings together experts in Android, Web and Cloud technologies to Manila for sessions, workshops and showcases. Our team is passionate in showcasing and enhancing the innate Filipino skill.
+    </p>
+  </div>
+  </section>
 
 `;
 
---
+++ src/components/stat-section/template.js
@@ -0,0 +1,9 @@
+const template = (html) => html`
+
+  <section class="section">
+    STAT SECTION
+  </section>
+
+`;
+
+export { template };
