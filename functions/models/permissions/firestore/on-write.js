d941bd02208b1b828684dd2593fc833bad589b12
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-10-07 02:30:52 +0800

    changed permission model
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- functions/models/permissions/firestore/on-write.js
+++ functions/models/permissions/firestore/on-write.js
@@ -9,19 +9,83 @@ const onWrite = admin => (change, context) => {
   const updates = {};
 
   if (oldDocument) { // remove old data
-    const { permissions } = oldDocument;
-    if (permissions && permissions.length) {
-      for (let permission of permissions) {
-        updates[`${path}/${permission}`] = false;
+    const {
+      writeDatabase,
+      readDatabase,
+      writeFirestore,
+      readFirestore,
+      writeStorage,
+      readStorage
+    } = oldDocument;
+    if (writeDatabase && writeDatabase.length) {
+      for (let permission of writeDatabase) {
+        updates[`${path}/writeDatabase/${permission}`] = false;
+      }
+    }
+    if (readDatabase && readDatabase.length) {
+      for (let permission of readDatabase) {
+        updates[`${path}/readDatabase/${permission}`] = false;
+      }
+    }
+    if (writeFirestore && writeFirestore.length) {
+      for (let permission of writeFirestore) {
+        updates[`${path}/writeFirestore/${permission}`] = false;
+      }
+    }
+    if (readFirestore && readFirestore.length) {
+      for (let permission of readFirestore) {
+        updates[`${path}/readFirestore/${permission}`] = false;
+      }
+    }
+    if (writeStorage && writeStorage.length) {
+      for (let permission of writeStorage) {
+        updates[`${path}/writeStorage/${permission}`] = false;
+      }
+    }
+    if (readStorage && readStorage.length) {
+      for (let permission of readStorage) {
+        updates[`${path}/readStorage/${permission}`] = false;
       }
     }
   }
 
   if (document) { // onCreate or onUpdate
-    const { permissions } = document;
-    if (permissions && permissions.length) {
-      for (let permission of permissions) {
-        updates[`${path}/${permission}`] = true;
+    const {
+      writeDatabase,
+      readDatabase,
+      writeFirestore,
+      readFirestore,
+      writeStorage,
+      readStorage
+    } = document;
+    if (writeDatabase && writeDatabase.length) {
+      for (let permission of writeDatabase) {
+        updates[`${path}/writeDatabase/${permission}`] = true;
+      }
+    }
+    if (readDatabase && readDatabase.length) {
+      for (let permission of readDatabase) {
+        updates[`${path}/readDatabase/${permission}`] = true;
+      }
+    }
+    if (writeFirestore && writeFirestore.length) {
+      for (let permission of writeFirestore) {
+        updates[`${path}/writeFirestore/${permission}`] = true;
+      }
+    }
+    if (readFirestore && readFirestore.length) {
+      for (let permission of readFirestore) {
+        updates[`${path}/readFirestore/${permission}`] = true;
+      }
+    }
+    if (writeStorage && writeStorage.length) {
+      for (let permission of writeStorage) {
+        updates[`${path}/writeStorage/${permission}`] = true;
+      }
+    }
+    if (readStorage && readStorage.length) {
+      for (let permission of readStorage) {
+        updates[`${path}/readStorage/${permission}`] = true;
       }
     }
   }
